55b9c359060f799766919d4afc948d9f4d3b17fb
nemoNoboru <felipetavres@gmail.com>
2017-04-16 20:52:53 +0200

    sliding

--- src/discover/boopcard/boopcard.js
+++ src/discover/boopcard/boopcard.js
@@ -11,28 +11,29 @@ class BoopCard extends Component {
 
   render(){
     return(
-      <ScrollView style={{flex:1,marginBottom:40}}>
-        <Image source={placeholder} style={{flex:1}}/>
-        <Text style={{color:'#000',fontSize:30,padding:30,paddingBottom:4,fontWeight:'800'}}>{this.props.data.nombre}</Text>
-        <Text style={{color:'#000',fontSize:28,paddingLeft:30,fontWeight:'400'}}>{this.props.data.descripcion}</Text>
-        <Space/>
-        <Text style={{textAlign:'center',color:'#ff0000',fontSize:26,paddingLeft:30,fontWeight:'200'}}>dentro de 3 dias</Text>
-        <Space/>
+      <View style={{width:'100%',flex:1}}>
+      <Image width='100%' height='100' source={placeholder} style={{position:'absolute',top:0}}/>
+        <ScrollView style={{flex:1,marginBottom:40}}>
+          <View style={{marginTop:300,backgroundColor:'#fafafa'}}>
+          <Text style={{color:'#000',fontSize:30,padding:30,paddingBottom:4,fontWeight:'800'}}>{this.props.data.nombre}</Text>
+          <Text style={{color:'#000',fontSize:25,paddingLeft:30,fontWeight:'400'}}>{this.props.data.descripcion}</Text>
+          <Space/>
+          <Text style={{textAlign:'center',color:'#ff0000',fontSize:26,paddingLeft:30,fontWeight:'200'}}>dentro de 3 dias</Text>
+          <View style={{height:250}}/>
+          </View>
+        </ScrollView>
         <View style={likeButtons}>
           <DislikeButton onPress={this.props.onDislike}/>
           <LikeButton/>
         </View>
-        <Space/>
-      </ScrollView>
+      </View>
     )
   }
 }
 
 var likeButtons = {
-  //position:'absolute',
-  //bottom:0,
-  flex:1,
-
+  position:'absolute',
+  bottom:75,
   flexDirection:'row',
   justifyContent:'space-around',
   width: "100%",
